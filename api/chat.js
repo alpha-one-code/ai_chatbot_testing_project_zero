6870c23f2a4f7a22547407afb28df77c2bd1f1dc
alpha-one-code <areeb1996@gmail.com>
2025-08-03 12:57:16 +0530

    Update chat.js
    changed model to gpt-4.1-nano-2025-04-14

--- api/chat.js
+++ api/chat.js
@@ -17,7 +17,7 @@ module.exports = async (req, res) => {
 
   try {
     const chatCompletion = await openai.chat.completions.create({
-      model: "gpt-3.5-turbo",
+      model: "gpt-4.1-nano-2025-04-14",
       messages: [{ role: "user", content: message }],
     });
 
